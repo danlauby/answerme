582509e7d936420e2a735a0b569d38d5de4113b7
danlauby <dmlauby@gmail.com>
2017-03-24 10:10:35 -0700

    Display all questions on index.hbs and install bootstrap

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,6 +1,8 @@
 import Ember from 'ember';
 
+
 export default Ember.Route.extend({
-  questions: this.store.findAll('question'),
-  answers: this.store.finadAll('answers')
+  model() {
+    return this.store.findAll('question');
+  },
 });
