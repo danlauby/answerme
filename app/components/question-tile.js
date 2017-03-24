051d401e40f5c79269ff1daf4a0c803177935e5e
danlauby <dmlauby@gmail.com>
2017-03-24 10:39:07 -0700

    Toggle content of each question

--- app/components/question-tile.js
+++ app/components/question-tile.js
@@ -1,4 +1,14 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-});
+  questionContentShowing: false,
+   actions: {
+     toggleContent: function() {
+       if (!this.questionContentShowing) {
+         this.set('questionContentShowing', true);
+       } else {
+         this.set('questionContentShowing', false);
+       }
+     }
+   }
+ });
