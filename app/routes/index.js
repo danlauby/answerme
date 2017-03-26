753522a87e122fe99c048109ff4d76b6692c0e4b
danlauby <dmlauby@gmail.com>
2017-03-25 17:40:12 -0700

    Add answers to model in index.js and refactor html/bootsrtap in index.hbs

--- app/routes/index.js
+++ app/routes/index.js
@@ -14,20 +14,5 @@ export default Ember.Route.extend({
       newQuestion.save();
       this.transitionTo('index');
     }
-    // ,
-    // saveAnswer(params) {
-    //   var newAnswer = this.store.createRecord('answer', params);
-    //   newAnswer.save();
-    //   this.transitionTo('index');
-    // },
-    // update(question, params) {
-    //   Object.keys(params).forEach(function(key) {
-    //     if(params[key]!==undefined) {
-    //       question.set(key,params[key]);
-    //     }
-    //   });
-    //   question.save();
-    //   this.transitionTo('index');
-    // },
   }
 });
