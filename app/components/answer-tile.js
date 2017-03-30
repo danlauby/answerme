509b4a9046631aac74b8939450b22daa311bb087
danlauby <dmlauby@gmail.com>
2017-03-30 08:22:59 -0700

    Enlarge home link, set forms to empty strings and add form validation

--- app/components/answer-tile.js
+++ app/components/answer-tile.js
@@ -4,10 +4,13 @@ export default Ember.Component.extend({
   actions: {
     save: function() {
       var params = {
-        author: this.get('author'),
-        content: this.get('content'),
+        author: this.get('author') ? this.get('author'): '',
+        content: this.get('content') ? this.get('content'): '',
         question: this.get('question')
       };
+      this.set('author', '');
+      this.set('content', '');
+      this.set('question', '');
       this.sendAction('save', params);
     },
   }
--- app/components/new-question.js
+++ app/components/new-question.js
@@ -12,11 +12,14 @@ actions: {
   },
     saveQuestion() {
       var params = {
-        author: this.get('author'),
-        content: this.get('content'),
-        notes: this.get('notes'),
+        author: this.get('author') ? this.get('author'): '',
+        content: this.get('content') ? this.get('content'): '',
+        notes: this.get('notes') ? this.get('notes'): '',
         answers: this.get('answer')
       };
+      this.set('author', '');
+      this.set('content', '');
+      this.set('notes', '');
       this.set('addNewQuestion', false);
       this.sendAction('saveQuestion', params);
     }
